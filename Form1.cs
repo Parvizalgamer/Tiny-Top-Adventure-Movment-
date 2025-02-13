012004fc004dfb41bf2bd42aa2ea7f9fad2273ff
Abel Binu Varghese <242492@student.wqe.ac.uk>
2025-02-13 14:43:09 +0000

    added rsptie images to repostiry

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Tiny_Top_Adventure
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
